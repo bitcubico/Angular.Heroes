a2121cd54ca03045e2d6f546a4d5a64d8364e243
BITCUBICO Technology <bitcubico@gmail.com>
2020-11-26 09:12:35 -0500

    Agrego la lógica para obtener los datos de los heroes a traves de un servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { PageNotFoundComponent } from './components/error/page-not-found/page-not-found.component';
+import { HeroesService } from './services/heroes.service';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,9 @@ import { PageNotFoundComponent } from './components/error/page-not-found/page-no
     BrowserAnimationsModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
